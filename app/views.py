47f9cfadade7666328dadd1db262515057b720e7
felipemp <felipematheuspinto27@gmail.com>
2021-01-19 21:29:51 -0300

    First test commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+
+# Create your views here.
+def homepage(request):
+    hour = datetime.now().strftime(format='%d/%m/%Y %H:%M:%S')
+    return HttpResponse(f"Hey, this is a <b>test</b> web page, the last refresh was made at <b>{hour}</b>")
