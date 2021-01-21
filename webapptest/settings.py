84170718f3b3bb878fc3fe79a3926fa24c1fddd9
felipemp <felipematheuspinto27@gmail.com>
2021-01-21 16:58:36 -0300

    Trying with venv req.txt

--- webapptest/settings.py
+++ webapptest/settings.py
@@ -16,7 +16,7 @@ import json
 import django_heroku
 from django.core.exceptions import ImproperlyConfigured
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,10 +40,9 @@ def get_secret(setting, secrets=secrets):
 SECRET_KEY = get_secret('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True#False
 DEBUG = True#False
 
-ALLOWED_HOSTS = ["webapptest27.herokuapp.com"]
+ALLOWED_HOSTS =  []#["webapptest27.herokuapp.com"]
 
 # Application definition
 
@@ -136,6 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
