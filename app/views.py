e6197d6c2d9b0c95cee2c26afeb5b3c23db6c7d1
felipemp <felipematheuspinto27@gmail.com>
2021-01-21 18:01:17 -0300

    editing to run jo

--- app/views.py
+++ app/views.py
@@ -6,4 +6,5 @@ from datetime import datetime
 # Create your views here.
 def homepage(request):
     hour = datetime.now().strftime(format='%d/%m/%Y %H:%M:%S')
-    return HttpResponse(f"Hey, this is a <b>test</b> web page, the last refresh was made at <b>{hour}</b>")
+    s = f"Hey JOVAAAAAAAAAAAAAAANIA, this is a <b>test</b> web page, the last refresh was made at <b>{hour}</b>"
+    return HttpResponse(s)
